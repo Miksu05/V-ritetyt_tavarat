54088d80639ab7beafcb0ca18e54724e9335b8b7
mikae <mikae@MIKAEL>
2023-10-09 16:19:59 +0300

    Add project files.

---
+++ Väritetyt_tavarat/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+public class Tavara
+{
+    public string Nimi { get; set; }
+
+    public Tavara(string nimi)
+    {
+        Nimi = nimi;
+    }
+
+    public override string ToString()
+    {
+        return Nimi;
+    }
+}
+
+public class VariettyTavara<T>
+{
+    public T Tavara { get; }
+    public ConsoleColor Vari { get; }
+
+    public VariettyTavara(T tavara, ConsoleColor vari)
+    {
+        Tavara = tavara;
+        Vari = vari;
+    }
+
+    public void NaytaTavara()
+    {
+        Console.ForegroundColor = Vari;
+        Console.WriteLine(Tavara);
+        Console.ResetColor();
+    }
+}
+
+public class Miekka { }
+
+public class Kirves { }
+
+public class Jousi { }
+
+class Program
+{
+    static void Main()
+    {
+        // Luodaan väritettyjä tavaroita
+        var punainenMiekka = new VariettyTavara<Miekka>(new Miekka(), ConsoleColor.Red);
+        var sininenKirves = new VariettyTavara<Kirves>(new Kirves(), ConsoleColor.Blue);
+        var vihreäJousi = new VariettyTavara<Jousi>(new Jousi(), ConsoleColor.Green);
+
+        // Tulostetaan väritetyt tavarat
+        punainenMiekka.NaytaTavara();
+        sininenKirves.NaytaTavara();
+        vihreäJousi.NaytaTavara();
+
+        Console.ReadLine();
+    }
+}
